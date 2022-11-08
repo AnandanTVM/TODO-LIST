2599e63ecaf73d719d1732844630a18936331b9a
Anandan <anandan2016@gmail.com>
2022-11-08 19:40:12 +0530

    todo first commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+
+import './App.css';
+import { useState } from 'react'
+function App() {
+  let daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
+  let day = daylist[new Date().getDay()]
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
+  console.log(toDos);
+  return (
+
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {day} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+
+        {
+          toDos.map((data) => {
+            return (<div className="todo">
+              <div className="left">
+                <input onChange={(e) => {
+
+                  setToDos(toDos.filter(getdata => {
+                    if (getdata.id === data.id) {
+                      getdata.status = e.target.checked
+                    }
+                    return getdata
+                  }))
+                }} value={data.status} type="checkbox" name="" id="" />
+                <p>{data.text}</p>
+              </div>
+              <div className="right">
+                <i onClick={(e) => {
+
+                  console.log(data.id);
+                  setToDos(toDos.filter(getdata => {
+                    if (getdata.id !== data.id) {
+                      console.log(toDos);
+
+                    }
+                    return getdata
+                  }))
+                }} className="fas fa-times"></i>
+              </div>
+            </div>)
+
+          })
+        }
+        <h1>Completed Task</h1>
+        {toDos.map((data) => {
+          if (data.status) {
+            return (<h1>
+              {data.text}
+            </h1>)
+          }
+          return null
+
+        })}
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
